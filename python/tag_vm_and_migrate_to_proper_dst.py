71387495e93620aa3632aac0527edd59ce63f143
shenjiatong <shenjiatong@inspur.com>
2018-11-15 11:00:35 +0800

    add script for tagging and live migrating

---
+++ python/tag_vm_and_migrate_to_proper_dst.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python env
+# -*- coding: utf-8 -*-
+
+import sys
+import logging
+
+
+def read_config(file_path):
+    pass
+
+
+def tag_vm(nova_client, vms):
+    pass
+
+
+def main():
+    return 0
+
+
+if __name__ == "__main__":
+    exit()
