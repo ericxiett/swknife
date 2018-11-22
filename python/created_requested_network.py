d060bdd1c36a2b5920b21ca7a23d99069c82236a
shenjiatong <shenjiatong@inspur.com>
2018-11-22 15:47:15 +0800

    add host_routes and output to excel

--- python/created_requested_network.py
+++ python/created_requested_network.py
@@ -9,11 +9,12 @@ import logging
 from neutronclient.v2_0 import client
 from keystoneauth1 import session, loading
 from xlrd import open_workbook
+import xlwt
 
 DEFAULT_SPEC = "general"
 LOGGER = None
 COLUME_NAME = ["service", "network_name", "subnet_name", "cidr", "ip_pool", "vlanid", "disable_dhcp", 'disable_gw',
-               'gw', 'dns']
+               'gw', 'dns', 'host_routes']
 MAX_COLUMNS = len(COLUME_NAME)
 DEFAULT_NIC = "physnet1"
 
@@ -36,7 +37,7 @@ class Network(object):
     """
 
     def __init__(self, service, network_name, subnet_name, cidr, ip_pool, vlanid, disable_dhcp=u'否', disable_gw=u'否',
-                 gw=None, dns=None):
+                 gw=None, dns=None, host_routes=None):
         """
 
         :param service:
@@ -52,9 +53,11 @@ class Network(object):
         """
 
         # fixme input error?
+        self.network_id = None
         self.service = service
         self.network_name = network_name
         self.subnet_name = subnet_name
+        self.subnet_id = None
         self.cidr = cidr
 
         # disable dhcp
@@ -74,6 +77,7 @@ class Network(object):
 
         self.gw = gw
         self.dns = dns
+        self.host_routes = host_routes
 
     def create_network(self, neutron_client):
 
@@ -95,15 +99,17 @@ class Network(object):
         else:
             raise Exception("There are multiple networks with same name, please check config")
 
+        self.network_id = network.get('id')
         # create subnet if not existings
-        logger.info("network id: %s", network.get('id'))
+        logger.info("network id: %s", self.network_id)
         subnets = neutron_client.list_subnets(**{
             "network_id": network.get('id')
         }).get('subnets')
 
         logging.info("subnets obtained %s", str(subnets))
         if len(subnets) == 0:
-            neutron_client.create_subnet(self.get_subnet(network.get('id')))
+            subnet = neutron_client.create_subnet(self.get_subnet(network.get('id'))).get('subnet')
+            self.subnet_id = subnet.get('id')
         else:
             logger.warning("subnet: %s is duplicate, maybe it has been created already?", self.subnet_name)
 
@@ -149,6 +155,12 @@ class Network(object):
             pool = self.ip_pool.replace(u'，', ',').replace(u'；', ";").rstrip(';')
             subnet['subnet'].update(
                 {"allocation_pools": [dict_zip(i.split(',')[0:2], ['start', 'end']) for i in pool.split(';')]})
+
+        # https://developer.openstack.org/api-ref/network/v2/index.html?expanded=create-network-detail,create-subnet-detail#create-subnet
+        if self.gw and self.host_routes:
+            host_routes = [{"destination": i, "nexthop": self.gw} for i in
+                           self.host_routes.replace(u'，', ',').split(',')]
+            subnet['subnet'].update({"host_routes": host_routes})
 
         return subnet
 
@@ -272,6 +284,29 @@ def init_neutron_client(credentials):
     return nova_client
 
 
+def write_output_to_excel(networks, filename):
+    """
+    write networks output to filename
+    :param networks:
+    :param filename:
+    :return:
+    """
+
+    workbook = xlwt.Workbook()
+    sheet = workbook.add_sheet("networks")
+
+    attrs = ["service", 'network_id', "network_name", "subnet_id", "subnet_name"]
+
+    for i in range(len(attrs)):
+        sheet.write(0, i, attrs[i])
+
+    for j in range(len(networks)):
+        for i in range(len(attrs)):
+            sheet.write(1 + j, i, getattr(networks[j], attrs[i], ''))
+
+    workbook.save(filename)
+
+
 def get_parser():
     import argparse
 
@@ -280,6 +315,7 @@ def get_parser():
                         help='path to the configuration file')
     parser.add_argument('-d', '--debug', dest='debug', action='store_const', const=True,
                         default=False, help='enable debugging')
+    parser.add_argument('-o', '--ouput', dest='output_file', default="networks.xls", help='write output to file')
 
     return parser.parse_args()
 
@@ -307,10 +343,14 @@ def main():
     # create requested flavors
     create_networks(neutron_client, networks)
 
+    # output to excel
+    if not parser.output_file:
+        return 0
+    write_output_to_excel(networks, parser.output_file)
+
     return 0
 
 
 if __name__ == "__main__":
-
     # fixme init project?
     exit(main())
